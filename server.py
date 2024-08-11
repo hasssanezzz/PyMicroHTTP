ccf39eed6c4ec6574ad473e5bf0a8da63c7400ac
Hassan Ezz <dhassanezz98@gmail.com>
2024-08-11 14:03:27 +0300

    did some cleaning

--- server.py
+++ server.py
@@ -13,13 +13,25 @@ class Server:
             server_socket.bind((self.host, self.port))
             server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             server_socket.listen()
-            print(f"Server listening on {self.host}:{self.port}")
+            print(f'Server listening on {self.host}:{self.port}')
             while True:
                 conn, addr = server_socket.accept()
-                print(f"Connected by {addr}")
-                threading.Thread(target=self.handleConnection, args=(conn, addr)).start()
+                print(f'Connected by {addr}')
+                threading.Thread(target=self.__handleConnection, args=(conn, addr)).start()
 
-    def handleConnection(self, conn: socket.socket, addr):
+    def registerHandler(self, path: str, handler):
+        if not self.__isPathValid(path):
+            raise ValueError('invalid provided path:', path)
+        self.routes[path] = handler
+
+    def register(self, path):
+        if not self.__isPathValid(path):
+            raise ValueError('invalid provided path:', path)
+        def decorator(func):
+            self.routes[path] = func
+        return decorator
+
+    def __handleConnection(self, conn: socket.socket, addr):
         with conn:
             while True:
                 data = conn.recv(1024 * 2)
@@ -27,60 +39,56 @@ class Server:
                     break
 
                 reqstr = data.rstrip(b'\x00').decode()
-                parsed = self.parseRequest(reqstr)
+                parsed = self.__parseRequest(reqstr)
                 pathkey = f'{parsed["verb"]} {parsed["path"]}'
 
                 if pathkey in self.routes:
                     result = self.routes[pathkey](parsed)
-
                     if not isinstance(result, tuple):
-                        return conn.sendall(self.writeReponse(self.checkIfResultIsDict(result)))
-
+                        return conn.sendall(self.__writeReponse(self.__checkIfResultIsDict(result)))
                     resultLen = len(result)
                     if resultLen == 3:
                         response, code, headers = result
-                        return conn.sendall(self.writeReponse(self.checkIfResultIsDict(response), code, headers))
+                        return conn.sendall(self.__writeReponse(self.__checkIfResultIsDict(response), code, headers))
                     if resultLen == 2:
                         response, code = result
-                        return conn.sendall(self.writeReponse(self.checkIfResultIsDict(response), code))
-
+                        return conn.sendall(self.__writeReponse(self.__checkIfResultIsDict(response), code))
                     # 500 - invalid number of return values
-                    conn.sendall(self.writeReponse('', 500))
-                    raise ValueError("internal server error: handler returned bad number of values:", resultLen)
-
+                    conn.sendall(self.__writeReponse('', 500))
+                    raise ValueError('internal server error: handler returned bad number of values:', resultLen)
                 # 404 - route not found
-                return conn.sendall(self.writeReponse('', 404))
+                return conn.sendall(self.__writeReponse('', 404))
 
-    def checkIfResultIsDict(self, result):
+    def __checkIfResultIsDict(self, result):
         if isinstance(result, dict):
             return json.dumps(result)
         return result
 
-    def writeReponse(self, resp, code = 200, headers = {}, contentType = "application/json"):
+    def __writeReponse(self, resp, code = 200, headers = {}, contentType = 'application/json'):
         resp = str(resp)
         codeMessage = responses.get(code, 'Eshta')
         httpResp = (
-            f"HTTP/1.1 {code} {codeMessage}\r\n"
-            f"Content-Type: {contentType}\r\n"
-            f"Content-Length: {len(resp)}\r\n"
+            f'HTTP/1.1 {code} {codeMessage}\r\n'
+            f'Content-Type: {contentType}\r\n'
+            f'Content-Length: {len(resp)}\r\n'
         )
 
         for header, value in headers.items():
             header, value = str(header), str(value)
-            httpResp += f"{header}: {value}\r\n"
+            httpResp += f'{header}: {value}\r\n'
 
         return (httpResp + (
-            "\r\n"
-            f"{resp}"
+            '\r\n'
+            f'{resp}'
         )).encode()
 
-    def parseRequest(self, reqstr: str):
-        sep = "\r\n"
+    def __parseRequest(self, reqstr: str):
+        sep = '\r\n'
         head, body = reqstr.split(sep + sep)
         headlines = head.split(sep)
         head = headlines[0]
         headlines = headlines[1:]
-        verb, path, _ = head.split(" ")
+        verb, path, _ = head.split(' ')
         headers = {}
         for line in headlines:
             idx = line.index(':')
@@ -94,18 +102,6 @@ class Server:
             'body': body
         }
 
-    def isPathValid(self, path: str) -> bool:
-        pattern = r"^(GET|POST|PUT|DELETE|HEAD|OPTIONS|PATCH) /[^\s]*$"
+    def __isPathValid(self, path: str) -> bool:
+        pattern = r'^(GET|POST|PUT|DELETE|HEAD|OPTIONS|PATCH) /[^\s]*$'
         return bool(re.match(pattern, path))
-
-    def registerHandler(self, path: str, handler):
-        if not self.isPathValid(path):
-            raise ValueError('invalid provided path:', path)
-        self.routes[path] = handler
-
-    def register(self, path):
-        if not self.isPathValid(path):
-            raise ValueError('invalid provided path:', path)
-        def decorator(func):
-            self.routes[path] = func
-        return decorator
